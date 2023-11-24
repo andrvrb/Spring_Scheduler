28abfb0080c2db7587cae4a5f7700dc51ce58f61
AVVorobev <avorobyev@egartech.ru>
2023-11-24 16:44:38 +0300

    step 2. Выполнение задач с фиксированной скоростью

--- src/main/java/ru/egartech/service/ScheduledTaskService.java
+++ src/main/java/ru/egartech/service/ScheduledTaskService.java
@@ -2,7 +2,6 @@ package ru.egartech.service;
 
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -18,19 +17,32 @@ public class ScheduledTaskService {
  * */
 
 // fixedDelay - время задержки  в миллисекундах
-    @Scheduled(fixedDelay = 1000)
+/*    @Scheduled(fixedDelay = 1000)
     public void scheduleFixedDelayTask() throws InterruptedException {
         log.info("scheduleFixedDelayTask: begin");
         Thread.sleep(2000);
         log.info("scheduleFixedDelayTask: end");
-    }
+    }*/
 
 // initialDelay задержка перед первым выполнением метода
-    @Scheduled(fixedDelay = 1000, initialDelay = 10000)
+/*    @Scheduled(fixedDelay = 1000, initialDelay = 10000)
     public void scheduleFixedRateWithInitialDelayTask() throws InterruptedException {
         log.info("scheduleFixedRateWithInitialDelayTask: begin");
         Thread.sleep(3000);
         log.info("scheduleFixedRateWithInitialDelayTask: end");
-    }
+    }*/
 
+
+    /**
+     * задачи запускаются ровно через определённый промежуток времени
+     * и не ожидают завершения друг друга, при условии что планировщик задач их не запускает в одном потоке
+     * */
+    //fixedRate - время, через которое будет запущена новая задача
+    //значение в fixedRate больше чем значение в Thread.sleep, то есть работа метода укладывается в заданный интервал
+    @Scheduled(fixedRate = 3000)
+    public void scheduleFixedRateTask() throws InterruptedException {
+        log.info("scheduleFixedRateTask: begin");
+        Thread.sleep(1000);
+        log.info("scheduleFixedRateTask: end");
+    }
 }
